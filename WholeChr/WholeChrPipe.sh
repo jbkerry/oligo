5a1fdf3054d2a5da5049d0ff8ffe40c3faf63ac1
Jon Kerry <jonbkerry@gmail.com>
2016-12-15 12:19:43 +0000

    WholeChrPipe ready for first test with user input

--- WholeChr/WholeChrPipe.sh
+++ WholeChr/WholeChrPipe.sh
@@ -1,7 +1,11 @@
 #!usr/bin/bash
-python /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/CaptureOligos_WholeChromosome.py -g mm9 -c 11 -e HindIII -o 70
-bash /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/SplitFA.sh Oligos_mm9_chr11_HindIII_70bp.fa
-cd mm9_chr11_HindIII_70bp/
-python /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/MakeShells.py -g mm9 -c 11 -u 80000
+python /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/CaptureOligos_WholeChromosome.py -g $Genome -c $Chr -e $Enzyme -o $Oligo
+FastaName="Oligos_"$Genome"_chr"$Chr"_"$Enzyme"_"$Oligo"bp.fa"
+DirName=${FastaName:7:$((${#FastaName}-10))}
+lines=$(wc -l < ../$FastaName) # store number of lines
+Max=$((((($lines/2)/20000)+1)*20000))
+bash /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/SplitFA.sh $FastaName
+cd $DirName
+python /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/MakeShells.py -g $Genome -c $Chr -u $Max
 mkdir Logs
-bash /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/QsubShells.sh 80000
+bash /t1-data1/WTSA_Dev/jkerry/OligoDesign/WholeChr/QsubShells.sh $Max
