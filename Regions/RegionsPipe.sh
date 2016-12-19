21f07caf8608d901da756820967748d54e567b69
Jon Kerry <jonbkerry@gmail.com>
2016-12-19 12:20:31 +0000

    pre changing DepthGaugepl to DepthGauge.py

--- Regions/RegionsPipe.sh
+++ Regions/RegionsPipe.sh
@@ -26,12 +26,15 @@ elif [ $STARvar == 1 ]
 then
     echo "Running through STAR..."
     /package/rna-star/2.5.1b/bin/STAR --runThreadN 4 --readFilesIn ./GeneratedOligos.fa --genomeDir /databank/igenomes/$organism/UCSC/$genome/Sequence/STAR/ --genomeLoad NoSharedMemory --outFilterMultimapScoreRange 1000 --outFilterMultimapNmax 100000 --outFilterMismatchNmax 110 --seedSearchStartLmax 4 --seedSearchLmax 20 --alignIntronMax 10 --seedPerWindowNmax 15 --seedMultimapNmax 11000 --winAnchorMultimapNmax 200 --limitOutSAMoneReadBytes 300000 --outFileNamePrefix GeneratedOligos_
+else
+    echo "Please choose 0 or 1 for running BLAT or STAR"
+    exit 1
 fi
 
 echo "Repeat masker..."
 repeatmasker -noint -s -species $species ./GeneratedOligos.fa
 
-#perl ./DepthGauge.pl
-#python /t1-home/nuffmed/jkerry/Python/MergeGeneAssociation.py
-#python /t1-home/nuffmed/jkerry/Python/CheckDoubledFrags.py
-#echo "All done. Check stats.txt to see number of successful fragments and oligos"
+perl ./DepthGauge.pl
+python /t1-home/nuffmed/jkerry/Python/MergeGeneAssociation.py
+python /t1-home/nuffmed/jkerry/Python/CheckDoubledFrags.py
+echo "All done. Check stats.txt to see number of successful fragments and oligos"
