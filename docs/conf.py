9887fbcf0c50c988276ce3b901e3323c17a0d5b0
Jon Kerry <jonbkerry@gmail.com>
2017-07-05 09:49:57 +0100

    conf.py edit

--- docs/conf.py
+++ docs/conf.py
@@ -111,10 +111,6 @@ html_sidebars = {
     ]
 }
 
-html_additional_pages = {
-    '**': 'about.html',
-}
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
