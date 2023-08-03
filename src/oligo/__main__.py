967fc1492fa4750f571052cc03925fdb6dff5081
Jon Kerry <jonbkerry@gmail.com>
2023-08-03 21:39:08 +0100

    Modularise command-line arguments and switch to click

--- src/oligo/__main__.py
+++ src/oligo/__main__.py
@@ -1,26 +1,4 @@
-import click
+from oligo.cmdline.commands import cli
 
-@click.group()
-@click.option("--config", "-cfg", type=click.Path(exists=True))
-@click.option("--genome", "-g", type=click.Choice(["hg18", "hg19", "hg38", "mm9", "mm10"]))
-@click.option("--fasta", "-f", type=click.Path(exists=True))
-@click.pass_context
-def cli(ctx, config, genome, fasta):
-    pass
-
-@cli.command()
-@click.option("--bed", "-b", required=True, type=click.STRING)
-def capture(bed):
-    click.echo('capture')
-
-@cli.command()
-def tiled():
-    click.echo('tiled')
-
-@cli.command("off-target")
-def off_target():
-    click.echo('tiled')
-
-# print("Hello, World!")
 if __name__ == "__main__":
     cli(obj={})
